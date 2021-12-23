2e1b47e43687682210ab95eccfb922fcf0da7f9d
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-12-22 22:05:30 -0500

    meta tags added

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+
+
+
+
 document.getElementById('resume').addEventListener('click', () => {
-    return 
+    console.log("you")
+ 
+    // return window.open('https://drive.google.com/file/d/1IbV7DHIE6vTdi-EBidxoo589k-1QrkR8/view?usp=sharing')
+    location.href = "https://drive.google.com/file/d/1IbV7DHIE6vTdi-EBidxoo589k-1QrkR8/view?usp=sharing"
 } )
